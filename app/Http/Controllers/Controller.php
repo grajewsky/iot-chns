08c075a62b4041db90e14a63286dc19849d4bad1
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2020-11-30 21:40:49 +0100

    Add getAuthUser() method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Models\User;
+use Illuminate\Validation\UnauthorizedException;
 
-class Controller extends BaseController
+class Controller extends \Laravel\Lumen\Routing\Controller
 {
-    //
+    public function getAuthUser(): User
+    {
+        $authUser = auth()->user();
+
+        $user = ($authUser instanceof User) ? $authUser : User::find($authUser->getAuthIdentifier());
+
+        if (is_null($user)) {
+            throw new UnauthorizedException();
+        }
+
+        return $user;
+    }
 }
